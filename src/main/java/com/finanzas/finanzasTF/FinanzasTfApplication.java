1a984491702456f99dba42cb0f939c3a1ed632f3
arian <arianscurze@gmail.com>
2020-10-14 22:09:06 -0500

    v3

--- src/main/java/com/finanzas/finanzasTF/FinanzasTfApplication.java
+++ src/main/java/com/finanzas/finanzasTF/FinanzasTfApplication.java
@@ -2,12 +2,26 @@ package com.finanzas.finanzasTF;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.web.servlet.WebMvcAutoConfiguration;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
 @SpringBootApplication
 public class FinanzasTfApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(FinanzasTfApplication.class, args);
+	}
+	@Bean
+	public WebMvcConfigurer corsConfigurer() {
+		return new WebMvcConfigurerAdapter() {
+			@Override
+			public void addCorsMappings(CorsRegistry registry) {
+				registry.addMapping("/**").allowedOrigins("*");
+			}
+		};
 	}
 
 }
